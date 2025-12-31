241aaf42106b0ca705d91a226b878595478a3adf
mhshefat99 <webdev.shefat@gmail.com>
2025-12-31 20:02:32 +0600

    Fixed blog image rendering issues

--- lib/blogs.ts
+++ lib/blogs.ts
@@ -13,7 +13,7 @@ const blogs: BlogType[] = [
       "A comprehensive guide to designing scalable systems as a fullstack developer",
     date: "23/12/2025",
     slug: "system-design-for-fullstack-developers",
-    image: "/images/blogs/system-design.PNG",
+    image: "/images/blogs/system-design.png",
   },
 
   {
@@ -22,7 +22,7 @@ const blogs: BlogType[] = [
       "Master advanced React design patterns including render props, compound components, hooks patterns, and more",
     date: "23/12/2025",
     slug: "advanced-design-patterns-for-react",
-    image: "/images/blogs/design-patterns.PNG",
+    image: "/images/blogs/design-patterns.png",
   },
   {
     title: "How I Create Scalable Fullstack Applications",
@@ -30,7 +30,7 @@ const blogs: BlogType[] = [
       "A practical walkthrough of building scalable fullstack apps using React, Next.js, TypeScript, Tailwind, Prisma, PostgreSQL, MongoDB, Redis, Clerk, Arcjet and more",
     date: "23/12/2025",
     slug: "how-i-create-scalable-fullstack-application",
-    image: "/images/blogs/scalable-application.PNG",
+    image: "/images/blogs/scalable-application.png",
   },
   {
     title: "How to Create Advanced Re-usable Components Like shadcn/ui",
@@ -38,7 +38,7 @@ const blogs: BlogType[] = [
       "Learn the patterns and techniques behind shadcn/ui by leveraging Radix UI primitives and building professional, accessible component libraries",
     date: "23/12/2025",
     slug: "how-to-create-advanced-reusable-components-like-shadcnui",
-    image: "/images/blogs/shadcn-component.PNG",
+    image: "/images/blogs/shadcn-component.png",
   },
   {
     title:
@@ -47,7 +47,7 @@ const blogs: BlogType[] = [
       "Deep dive into JavaScript Promises, from basics to advanced patterns, and how to implement polyfills for older browsers",
     date: "23/12/2025",
     slug: "mastering-javascript-promise-with-polyfills",
-    image: "/images/blogs/javascript-promise.PNG",
+    image: "/images/blogs/javascript-promise.png",
   },
 ];
 
